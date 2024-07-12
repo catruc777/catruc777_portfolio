fb10f52a4137ed548c4d0e2953252d6e7b30f989
catruc777 <ifresh.2004@gmail.com>
2024-07-12 17:59:58 +0900

    feat: Change avatar

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Catruc Andrei",
-  profile: "/profile.png",
+  profile: "/profile.jpg",
   designation: "Software Developer",
   description:
     "My name is CATRUC ANDREI. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and Python and I love to do most of the things using JavaScript and Python. I am available for any kind of job opportunity that suits my skills and interests.",
